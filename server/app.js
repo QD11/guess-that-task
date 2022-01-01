a87351c1f83e26184fdf91e252c04bb027729d2c
QuangDo <qdangdo@gmail.com>
2021-12-31 21:53:47 -0600

    add example

--- server/app.js
+++ server/app.js
@@ -7,7 +7,37 @@ app.get('/', (req, res) => {
     .send('Hello server is running')
     .end();
 });
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import "dotenv/config.js";
 
+import postsRoutes from './routes/posts.js'
+
+const app = express();
+//Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+//Import Routes
+app.use('/posts', postsRoutes);
+
+//ROUTES
+app.get('/', (req, res) => {
+    res
+        .send('We are on home')
+        .status(200)
+        .end()
+});
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, () => 
+    console.log('connected to DB!')
+)
+
+//How do we start listening to the server
+app.listen(3000, () => console.log('Server running on port: http://localhost:3000'));
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
