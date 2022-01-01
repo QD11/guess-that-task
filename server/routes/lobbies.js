0c8e49a66ec747daa86004915969b4c01273ec15
QuangDo <qdangdo@gmail.com>
2022-01-01 15:45:14 -0600

    unpopulate players

--- server/routes/lobbies.js
+++ server/routes/lobbies.js
@@ -7,7 +7,7 @@ const router = express.Router();
 //ALL Lobbys
 router.get('/', async (req, res) => {
     try{
-        const lobbies = await Lobby.find().populate("players");
+        const lobbies = await Lobby.find().populate;
         res.json(lobbies)
     }catch(err){
         res.json({message:err})
