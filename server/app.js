82ae294918451eaf5a25e8085413ff57c3d6cbb9
QuangDo <qdangdo@gmail.com>
2022-01-02 01:19:17 -0600

    add connection

--- server/app.js
+++ server/app.js
@@ -32,9 +32,25 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECTION, () => 
-    console.log('connected to DB!')
-)
+mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongo has connected succesfully')
+})
+mongoose.connection.on('reconnected', () => {
+    console.log('Mongo has reconnected')
+})
+mongoose.connection.on('error', error => {
+    console.log('Mongo connection has an error', error)
+mongoose.disconnect()
+})
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongo connection is disconnected')
+})
+// mongoose.connect()
 
 app.get('*', (request, response) => {
 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
