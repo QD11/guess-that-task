b0d0959c26ca61512290650d9dc60cfb0b4f9b36
QuangDo <qdangdo@gmail.com>
2022-01-03 15:39:37 -0600

    remove posts

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 //Import Routes
-app.use('/posts', postsRoutes);
+// app.use('/posts', postsRoutes);
 app.use('/players', playersRoutes);
 app.use('/lobbies', lobbiesRoutes);
 
