85faa375a7702c19bb888e8502f06e9743886871
QuangDo <qdangdo@gmail.com>
2022-01-06 01:26:00 -0600

    media query

--- client/pages/index.js
+++ client/pages/index.js
@@ -98,10 +98,10 @@ const Home = () => {
                     />
                     <div className="bottom-container">
                         <div className="create-container">
-                            <button onClick={createLobby} >Create a Lobby</button>
+                            <button onClick={createLobby} >Create Lobby</button>
                         </div>
                         <div className="join-container">
-                            <button onClick={() => setToggleJoinLobby(true)}>Join a Lobby</button>
+                            <button onClick={() => setToggleJoinLobby(true)}>Join Lobby</button>
                         </div>
                     </div>
                 </>
@@ -120,79 +120,85 @@ const Title = styled.h1`
 `
 
 const Center = styled.div`
-background: #ffd892;
-position: absolute;
-left: 50%;
-top: 50%;
-transform: translate(-50%, -50%);
-border-radius: 6px;
-padding: 50px;
-// min-height: 450px;
-min-width: 500px;
-& h1 {
-    text-align: center;
-}
-.name-input {
-    display: block;
-    margin-bottom: 1rem;
-    margin-right: auto;
-    margin-left: auto;
-    background: none;
-    outline: none;
-    width: 300px;
-    text-align: center;
-    font-weight: 200;
-    font-size: 25px;
-    border: none;
-    border-bottom: 2px solid #b17822;
-}
-.input-code {
-    display: block;
-    margin-bottom: 1rem;
-    margin-right: auto;
-    margin-left: auto;
-    margin-block-end: 0.67em;
-    background: #fff;
-    outline: none;
-    width: 300px;
-    text-align: center;
-    font-weight: 200;
-    font-size: 50px;
-    // border: none;
-}
-.bottom-container {
-    display: flex;
-    justify-content: space-between;
-    .middle-container {
-        border-right: 3px solid #1a141466;
-    }
-    .create-container {
-        .back-button{
-            background: red;
-            color: white;
-        }
-    }
-    & button {
-        font-size: 20px;
-        background: #fff no-repeat 50%;
-        background-size: 40px 40px;
-        // color: #404136;
-        padding: 10px 25px;
-        position: relative;
-        border-radius: 4px;
-        font-size: 20px;
-        font-weight: 600;
-        border: 0;
+    background: #ffd892;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 6px;
+    padding: 50px;
+    // min-height: 450px;
+    @media (min-width:320px){
         width: 100%;
-        // width: 10rem;
-        cursor: pointer;
+    }
+    @media (min-width:1024px){
+        width: 50%;
+    }
+
+    & h1 {
+        text-align: center;
+    }
+    .name-input {
+        display: block;
+        margin-bottom: 1rem;
+        margin-right: auto;
+        margin-left: auto;
+        background: none;
         outline: none;
-        transition: .3s ease-out;
-        :hover {
-            transform: scale(1.05);
+        width: 300px;
+        text-align: center;
+        font-weight: 200;
+        font-size: 25px;
+        border: none;
+        border-bottom: 2px solid #b17822;
+    }
+    .input-code {
+        display: block;
+        margin-bottom: 1rem;
+        margin-right: auto;
+        margin-left: auto;
+        margin-block-end: 0.67em;
+        background: #fff;
+        outline: none;
+        width: 300px;
+        text-align: center;
+        font-weight: 200;
+        font-size: 50px;
+        // border: none;
+    }
+    .bottom-container {
+        display: flex;
+        justify-content: space-between;
+        .middle-container {
+            border-right: 3px solid #1a141466;
+        }
+        .create-container {
+            .back-button{
+                background: red;
+                color: white;
+            }
+        }
+        & button {
+            font-size: 20px;
+            background: #fff no-repeat 50%;
+            background-size: 40px 40px;
+            // color: #404136;
+            padding: 10px 25px;
+            position: relative;
+            border-radius: 4px;
+            font-size: 20px;
+            font-weight: 600;
+            border: 0;
+            width: 100%;
+            // width: 10rem;
+            cursor: pointer;
+            outline: none;
+            transition: .3s ease-out;
+            :hover {
+                transform: scale(1.05);
+            }
         }
     }
-}
 `
 
 export default Home
