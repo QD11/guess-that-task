ce2733770d9631aa0b1de8ba03ec601117d0b3fb
QuangDo <qdangdo@gmail.com>
2022-01-17 20:17:19 -0600

    persist redux on refresh

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {persistStore, persistReducer, FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import userReducer from './userSlice'
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+}
 
 const store = configureStore({
     reducer: {
-        user: userReducer
-    }
+        user: persistReducer(persistConfig, userReducer)
+    },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }),
 })
 
+//let persistor = persistStore(store)
+
+export const persistor = persistStore(store)
 export default store;
