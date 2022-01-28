9026baddcdb0151f0bc849f89a60b61e9f5a76b0
QuangDo <qdangdo@gmail.com>
2022-01-28 03:00:43 -0600

    Add tasks

--- server/tasks.js
+++ server/tasks.js
@@ -34,7 +34,51 @@ const tasks = [
     {
         task: "Impersonate SpongeBob"
     },
-    
+    {
+        task: "Make a heart symbol with your hands and show it to an imposter"
+    },
+    {
+        task: "Blow kisses"
+    },
+    {
+        task: "Perform an Aegyo (a cute, over-the-top facial expression with sounds)"
+    },
+    {
+        task: "Say 'I love you' to an imposter"
+    },
+    {
+        task: "Kick your legs in the air and say 'Whooo!' "
+    },
+    {
+        task: "Jump up and clap your hands three times"
+    },
+    {
+        task: "Scrunch your nose 5 times"
+    },
+    {
+        task: "Use a pickup line three times (once if with an imposter)"
+    },
+    {
+        task: "Clap a slate"
+    },
+    {
+        task: "Perform a dance"
+    },
+    {
+        task: "Give a hug twice. Once if with an imposter"
+    },
+    {
+        task: "Drink 3 glasses of any beverage"
+    },
+    {
+        task: "Make the TT sign (Look up 'TT' by Twice)"
+    },
+    {
+        task: "Do the 'Shy Shy Shy' dance (Look up 'Shy Shy Shy' by Sana from Twice)"
+    },
+    {
+        task: "Eat a food and say 'Yummmmmmmmy'"
+    }
 ]
 
 export default tasks;
