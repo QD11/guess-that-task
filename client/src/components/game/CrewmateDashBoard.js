bd4b5bba9394db21797182d4904f6055fed5ea01
Quang Do <qdangdo@gmail.com>
2022-03-09 22:41:13 -0600

    console.log(players)

--- client/src/components/game/CrewmateDashBoard.js
+++ client/src/components/game/CrewmateDashBoard.js
@@ -17,6 +17,8 @@ const CrewmateDashBoard = ({imposters, crewmates, user}) => {
             alive: true,
         })
     }))
+
+    console.log(playersTasks)
     
     const [modal, setModal] = useState(false)
     const toggleModal = () => {
