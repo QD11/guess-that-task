8761525124998cba80222ee6512a1505b1e3e647
Quang Do <qdangdo@gmail.com>
2022-03-09 22:27:16 -0600

    add crewmateResponse for crewmate dhasboard

--- client/src/components/game/CrewmateDashBoard.js
+++ client/src/components/game/CrewmateDashBoard.js
@@ -45,7 +45,15 @@ const CrewmateDashBoard = ({imposters, crewmates, user}) => {
             }))
         })
         socket.on('crewmateResponse', data => {
-            console.log(data)
+            if (data.userResponse === true) {
+                setPlayersTasks(playersTasks => playersTasks.map(crewmate => {
+                    if (crewmate._id === data.user._id) {
+                        return({...crewmate, alive: false})
+                    } else {
+                        return(crewmate)
+                    }
+                }))
+            }
         })
 
         return () => {
