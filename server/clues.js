1f235092db96c7c7b582664f8b4442c0910fd482
Quang Do <qdangdo@gmail.com>
2022-03-24 01:34:11 -0500

    add clues

---
+++ server/clues.js
@@ -0,0 +1,14 @@
+const clues = [
+    {
+        id: 1,
+        clue: "All crewmates must say whether they have completed their tasks"
+    },
+    {
+        id: 2,
+        clue: "Pick 1/3 of the crewmates (rounded up). They must say whether they did their tasks in the last 5 minutes."
+    },
+    {
+        id: 3,
+        clue: "All crewmates who have finished their tasks in the last 5 minutes must do them again"
+    }
+]
